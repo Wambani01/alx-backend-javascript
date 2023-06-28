9af339d822b2ce6c2ef6ac4f13b1b21040d72540
Wambani01 <georgewmbn@gmail.com>
2023-06-28 05:54:17 -0700

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [];
@@ -11,18 +11,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled(promises)
     .then((settledPromises) => {
-      for (let i = 0; i < settledPromises.length; i++) {
+      for (let i = 0; i < settledPromises.length; i += 1) {
         const result = settledPromises[i];
-        const status = result.status;
+        const { status } = result;
         const value = status === 'fulfilled' ? result.value : result.reason;
 
         results.push({
-          status: status,
-          value: value
+          status,
+          value,
         });
       }
 
       return results;
     });
 }
-
