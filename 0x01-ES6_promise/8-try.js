24ee27389f6bd9a90fd89577564cefc914924754
Wambani01 <georgewmbn@gmail.com>
2023-06-28 10:38:49 -0700

    try catch in js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,11 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
+  try {
+    if (denominator === 0) {
     throw new Error('Cannot divide by 0');
   }
 
   return numerator / denominator;
+  } catch(error) {
+      throw new Error('Cannot divide by 0');
+  }
 }
