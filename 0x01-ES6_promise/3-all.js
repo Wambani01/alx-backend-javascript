0e95c85a2dab1fb8f7c206539e01cdf39acfd4fb
Wambani01 <georgewmbn@gmail.com>
2023-06-28 03:51:47 -0700

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const data1 = uploadPhoto()
-  const data2 = createUser()
+  const data1 = uploadPhoto();
+  const data2 = createUser();
   return Promise.all([data1, data2])
     .then((results) => {
       console.log(results[0].body, results[1].firstName, results[1].lastName);
