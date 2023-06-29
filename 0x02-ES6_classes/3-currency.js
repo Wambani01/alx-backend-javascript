8b1190139f4b0977938ec767998884d7d7851728
Wambani01 <108225824+Wambani01@users.noreply.github.com>
2023-06-29 13:23:00 +0300

    classmethods in js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,23 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   get name() {
     return this._name;
   }
+
   set code(value) {
     this._code = value;
   }
+
   set name(value) {
     this._name = value;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
