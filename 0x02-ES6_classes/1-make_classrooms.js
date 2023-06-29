6fda8c7d2d28f9e3053b753a7e3ebed125ac3c65
Wambani01 <108225824+Wambani01@users.noreply.github.com>
2023-06-29 12:39:00 +0300

    class instantiation

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
 import classRoom from "./0-classroom";
 export default function initializeRooms() {
-  return [new classRoom(19), new classRoom(19), new classRoom(34)];
+  return [new classRoom(19), new classRoom(20), new classRoom(34)];
 }
